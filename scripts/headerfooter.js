b451d4ea41a73716a8b6c4368e72aad1fb13d7fe
clerickbarrion <clerickbarrion@gmail.com>
2025-01-27 00:05:25 -0500

    template

--- scripts/headerfooter.js
+++ scripts/headerfooter.js
@@ -2,12 +2,14 @@ window.addEventListener("DOMContentLoaded", () => {
     const header = document.querySelector("header");
     header.innerHTML = `
         <div>
-            <h1>Clerick Barrion's Crazy Bat</h1>
+            <h1>Clerick Barrion's Crazy Bat | WEB215</h1>
         </div>
         <small><i></i></small>
         <nav>
             <ul>
                 <li><a href="./">Home</a></li>
+                <li><a href="introduction.html">Introduction</a></li>
+                <li><a href="contract.html">Contract</a></li>
             </ul>
         </nav>
     `;
@@ -16,13 +18,13 @@ window.addEventListener("DOMContentLoaded", () => {
         <ul>
             <li><a href="https://github.com/clerickbarrion" target="_blank">GitHub</a></li>
             <li><a href="https://clerickbarrion.github.io" target="_blank">GitHub.io</a></li>
-            <li><a href="https://clerickbarrion.github.io/web215" target="_blank">WEB115.io</a></li>
+            <li><a href="https://clerickbarrion.github.io/web215" target="_blank">WEB215.io</a></li>
             <li><a href="https://www.freecodecamp.org/clerick" target="_blank">freeCodeCamp</a></li>
             <li><a href="https://www.codecademy.com/profiles/cbarrion" target="_blank">Codecademy</a></li>
             <li><a href="https://jsfiddle.net/u/clerickbarrion/fiddles/" target="_blank">JSFiddle</a></li>
             <li><a href="https://www.linkedin.com/in/clerickbarrion" target="_blank">LinkedIn</a></li>
         </ul>
         <br>
-        <p>Page built by <a style="color: beige" href="clerickbarrion.html">Clerick Barrion</a> &copy;2025</p>
+        <p>Website designed by Clerick Barrion</p>
     `;
 });
